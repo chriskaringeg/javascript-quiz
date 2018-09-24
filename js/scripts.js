f03cdc91719b351f46d8bb584d099c9fb6660de1
BenOkwara <maranathabenson@gmail.com>
2018-09-24 15:25:35 +0300

    updated index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,19 @@
+var questions=function(question1,question2,question3,question4)
+return(question1+question2+question3+question4)
 (document).ready(function() {
-         $("#blanks form").submit(function() {
-          var qs1=$("input:radio[name=method]:checked").val();
-          var qs2=$("input:radio[name=operator]:checked").val();
-          var qs3=$("input:radio[name=dataType]:checked").val();
-          var qs4=$("input:radio[name=function]:checked").val();
-           blanks.forEach(function(blank) {
-             var userInput = $("input." + blank).val();
-             $("." + blank).text(userInput).val();
-           });
-
+  $("#script").submit(function() {
+          var qs1=parseInt($("input:radio[name=question1]:checked").val();))
+          var qs2=parseInt($("input:radio[name=question2]:checked").val();)
+          var qs3=parseInt($("input:radio[name=question3]:checked").val();)
+          var qs4=parseInt($("input:radio[name=question4]:checked").val();)
+          var scores=result(question1,question2,question3,question4)
+          if (scores>=70) {
+            total.innerHTML=("very good your score is " + scores)
+          }
+          else{
+            total.innerHTML=("fairly good your score is "+ scores)
+          }
+            $("#story").sho();
            $("#fifthp").show();
          });
        });
