3a8f829d1b1f7e9869d7c56b64c395bb9170b7e2
BenOkwara <maranathabenson@gmail.com>
2018-09-24 11:43:25 +0300

    changes in my README

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
-
-  (document).ready(function() {
+(document).ready(function() {
          $("#blanks form").submit(function() {
-           var blanks = ["qs", "qs", "qs", "qs","qs", "qs"];
-
+          var qs1=$("input:radio[name=method]:checked").val();
+          var qs2=$("input:radio[name=operator]:checked").val();
+          var qs3=$("input:radio[name=dataType]:checked").val();
+          var qs4=$("input:radio[name=function]:checked").val();
            blanks.forEach(function(blank) {
              var userInput = $("input." + blank).val();
              $("." + blank).text(userInput).val();
            });
 
-           $("#story").sho();
+           $("#fifthp").show();
          });
        });
 }
